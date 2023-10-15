68b621f541f4943b805ef3eb21276a9d49cc95a2
Mhd Fauzy <moha.fauzy09@gmail.com>
2023-10-15 20:19:56 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div class="relative w-[1352px] h-[75px] bg-red-600 shadow-xl mt-7">
+        <img src="assets/image/logo_happistudio 3.png" alt="" class="w-[130px] ml-20 absolute top-0 shadow-inner">
+    </div>
+    <div>
+        <img src="assets/image/bg.png" alt="">
+    </div>
+    <div class="relative w-[1352px] h-[400px] bg-white">
+        <img src="assets/image/logo_happistudio 4.png" alt="" class="w-[350px] ml-20 mt-10 absolute top-0">
+        <p class="flex justify-center text-xl font-bold mt-10"> ABOUT US</p>
+        
+    </div>
+    <div class="relative w-[1352px] h-[600px] bg-gradient-to-b from-white to-red-700">
+        <div class="card w-80 bg-base-100 shadow-xl ml-8 mt-6">
+            <figure class="px-5 pt-5">
+                <img src="assets/image/contoh1.png" alt="Shoes" class="rounded-lg w-[250px]" />
+            </figure>
+        <div class="card-body" >
+                <h2 class="card-title text-center text-red-500">SELF PHOTO FOR SINGLE</h2>
+            <div class="card-actions">
+                <button class=" btn btn-neutral">Lihat detail</button>
+            </div>
+        </div>
+        </div>
+    </div>
+</body>
+</html>

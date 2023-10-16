e06b6ad130cced5808f70e60e7ba2162003db283
Mhd Fauzy <moha.fauzy09@gmail.com>
2023-10-16 12:55:58 +0700

    membuat landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,27 +9,60 @@
 </head>
 <body>
     <div class="relative w-[1352px] h-[75px] bg-red-600 shadow-xl mt-7">
-        <img src="assets/image/logo_happistudio 3.png" alt="" class="w-[130px] ml-20 absolute top-0 shadow-inner">
+        <div>
+            <img src="assets/image/logo_happistudio 3.png" alt="" class="w-[130px] ml-20 absolute top-0 shadow-lg">
+        </div>
+        <div class="flex justify-center items-center">
+            <p class="font-bold text-black mt-6">BERANDA</p>
+            <p class="ml-5 font-bold text-white mt-6">PRICELIST</p>
+            <img src="assets/icon/arrow.png" alt="" class="filter brightness-0 invert mt-6"> 
+            <p class="ml-5 font-bold text-white mt-6">RESERVASI</p> 
+            <img src="assets/icon/arrow.png" alt="" class="filter brightness-0 invert mt-6"> 
+        </div>
     </div>
     <div>
-        <img src="assets/image/bg.png" alt="">
+        <img src="assets/image/bg.png" alt="" class="shadow-xl">
     </div>
     <div class="relative w-[1352px] h-[400px] bg-white">
         <img src="assets/image/logo_happistudio 4.png" alt="" class="w-[350px] ml-20 mt-10 absolute top-0">
         <p class="flex justify-center text-xl font-bold mt-10"> ABOUT US</p>
         
     </div>
-    <div class="relative w-[1352px] h-[600px] bg-gradient-to-b from-white to-red-700">
-        <div class="card w-80 bg-base-100 shadow-xl ml-8 mt-6">
-            <figure class="px-5 pt-5">
-                <img src="assets/image/contoh1.png" alt="Shoes" class="rounded-lg w-[250px]" />
-            </figure>
-        <div class="card-body" >
-                <h2 class="card-title text-center text-red-500">SELF PHOTO FOR SINGLE</h2>
+    <div class=" w-[1352px] h-[600px] bg-gradient-to-b from-white to-red-700">
+        <div class="flex justify-center">
+            <div class="card w-80 bg-base-100 shadow-xl mt-10 ml-10">
+                <figure class="px-5 pt-5">
+                    <img src="assets/image/contoh1.png" alt="Shoes" class="rounded-lg w-[250px]" />
+                </figure>
+            <div class="card-body items-center text-center mt-7" >
+                <h2 class="card-title text-red-500">SELF PHOTO FOR SINGLE</h2>
             <div class="card-actions">
                 <button class=" btn btn-neutral">Lihat detail</button>
             </div>
-        </div>
+            </div>
+            </div>
+            <div class="card w-80 bg-base-100 shadow-xl mt-10 ml-10">
+                <figure class="px-5 pt-5">
+                    <img src="assets/image/contoh2.jpg" alt="Shoes" class="rounded-lg w-[250px]" />
+                </figure>
+            <div class="card-body items-center text-center mt-5" >
+                <h2 class="card-title text-red-500">SELF PHOTO FOR DOUBLE</h2>
+            <div class="card-actions">
+                <button class=" btn btn-neutral">Lihat detail</button>
+            </div>
+            </div>
+            </div>
+            <div class="card w-80 bg-base-100 shadow-xl mt-10 ml-10">
+                <figure class="px-5 pt-5">
+                    <img src="assets/image/contoh3.png" alt="Shoes" class="rounded-lg w-[250px]" />
+                </figure>
+            <div class="card-body items-center text-center" >
+                <h2 class="card-title text-red-500">SELF PHOTO FOR GROUP</h2>
+            <div class="card-actions">
+                <button class=" btn btn-neutral">Lihat detail</button>
+            </div>
+            </div>
+            </div>
         </div>
     </div>
 </body>
